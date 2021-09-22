43de5fcda85915f9eb6df01c411a9ae38bf78823
Fabriany O <fabrianyo@gmail.com>
2021-09-22 10:51:49 -0500

    primir commit

---
+++ src/main/java/com/genios/GeniosApplication.java
@@ -0,0 +1,13 @@
+package com.genios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeniosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genios/GeniosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
